df3dbaa07f352856ec4d2b12f41118df9b39c242
wilydodo <wilydodo10@gmail.com>
2023-08-05 15:01:43 +0900

    english

--- class3.py
+++ class3.py
@@ -8,7 +8,7 @@ class God():
         print(f"my name is {self.name}, im {self.age} years old, Im from {self.country} and my height is {self.height}")
 
 a = God("minedapple",29,"korean",169.9)
-b = God("初音ミク",16,"日本",174)
+b = God("hatsune miku",16,"japan",174)
 c = God("a",1,"a",111)
 d = God("b",2,"b",222)
 e = God("c",3,"c",333)
